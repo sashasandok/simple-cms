a73e47d11bcd0c63942a108ff8127eacd3f16454
Alex Sandok <alex.pro.sandok@gmail.com>
2019-01-05 09:47:38 +0200

    creating layout with header and future links for routes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// react
+import React, { Component } from 'react'
+
+// styles
+import './App.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <div className="app">
+        <header className="app-header">
+          <div className="app-header-item">DUMMY TABLE</div>
+          <div className="app-header-item">DUMMY CHART</div>
+          <div className="app-header-item">DUMMY LIST</div>
         </header>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
